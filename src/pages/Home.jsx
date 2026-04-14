0a55fba1ab40e7a7c8fd51bb5f8de8b9ab3d54df
dev-code-0 <ballonabjose@gmail.com>
2026-04-14 01:02:00 -0500

    modific

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ActionButtons from '../components/ActionButtons';
 import TemplateCarousel from '../components/TemplateCarousel';
-import { AntiInspectGuard } from '../lib/antiInspect';
 import '../styles/home.css';
 
 export default function Home() {
   return (
-    <AntiInspectGuard>
+    <div>
       <main className="home-container">
         <h1 className="main-title-home">CODES FREE</h1>
         {/* Carrusel Dinámico */}
           <TemplateCarousel />
         <ActionButtons />
       </main>
-    </AntiInspectGuard>
+    </div>
   );
 }
